4bfce6bdb31197c01e42814d5b7b7b5ba792c106
Mark Dujali <mddujali@gmail.com>
2025-06-30 12:12:32 +0800

    Add api.ip-addresses.update endpoint

--- app/Http/Controllers/Api/IpAddressController.php
+++ app/Http/Controllers/Api/IpAddressController.php
@@ -78,4 +78,27 @@ class IpAddressController extends BaseController
 
         return $this->resolveResponse($response);
     }
+
+    public function update(SaveIpAddressRequest $request): JsonResponse
+    {
+        try {
+            $response = Http::asJson()
+                ->put(
+                    url: config('services.ip_service_api.url') . '/api/ip-addresses/' . $request->route('ip_address_id'),
+                    data: $request->validated()
+                );
+        } catch (ConnectionException) {
+            return $this->errorResponse(
+                status: Response::HTTP_SERVICE_UNAVAILABLE,
+                message: __('Failed to connect to the IP service.')
+            );
+        } catch (Exception) {
+            return $this->errorResponse(
+                status: Response::HTTP_INTERNAL_SERVER_ERROR,
+                message: __('Something went wrong, please try again later.')
+            );
+        }
+
+        return $this->resolveResponse($response);
+    }
 }
---
+++ tests/Feature/Api/IpAddresses/UpdateIpAddressTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature\Api\IpAddresses;
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use Tests\Feature\Api\BaseTestCase;
+
+class UpdateIpAddressTest extends BaseTestCase
+{
+    public function test_it_should_return_service_unavailable(): void
+    {
+        $path = 'tests/Fixtures/Api/IpAddresses/service-unavailable.json';
+        $body = file_get_contents(base_path($path));
+
+        Http::fake([
+            config('services.ip_service_api.url') . '/api/ip-addresses/1' => Http::response(
+                body: $body,
+                status: Response::HTTP_SERVICE_UNAVAILABLE
+            ),
+        ]);
+
+        $response = $this->json(
+            method: 'put',
+            uri: route('api.ip-addresses.update', ['ip_address_id' => 1]),
+            data: $this->requestData(),
+            headers: $this->headers
+        );
+
+        $response->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+        $response->assertExactJsonStructure([
+            'message',
+            'error_code',
+            'errors',
+        ]);
+    }
+
+    public function test_it_should_store_ip_address(): void
+    {
+        $path = 'tests/Fixtures/Api/IpAddresses/ip-address-edited.json';
+        $body = file_get_contents(base_path($path));
+
+        Http::fake([
+            config('services.ip_service_api.url') . '/api/ip-addresses/1' => Http::response(
+                body: $body,
+                status: Response::HTTP_OK
+            ),
+        ]);
+
+        $response = $this->json(
+            method: 'put',
+            uri: route('api.ip-addresses.update', ['ip_address_id' => 1]),
+            data: $this->requestData(),
+            headers: $this->headers
+        );
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertExactJsonStructure([
+            'message',
+            'data' => [
+                'id',
+                'ip_address',
+                'label',
+                'comment',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    private function requestData(): array
+    {
+        return [
+            'ip_address' => '150.82.242.195',
+            'label' => 'est - edited',
+            'comment' => 'Quo quia reiciendis sunt qui aut tenetur.',
+        ];
+    }
+}
